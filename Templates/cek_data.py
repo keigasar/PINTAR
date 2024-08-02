8907ab38fd091baf13b3e787b1ee59c32946ffae
keigasar <148850076+keigasar@users.noreply.github.com>
2024-08-02 15:53:26 +0700

    Delete Templates directory

--- Templates/cek_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-file_path = 'D:/0. Kantor/9. IKND/07. Pofiling BUMN/_Profil BUMD Per Provinsi/Dashboard/Database/004. Data Putusan BUMD.xlsx'
-
-try:
-    df = pd.read_excel(file_path, usecols=['Nama BUMD', 'Jenis Fraud', 'Tahun Persidangan', 'Status Kasus', 'No. SIPP', 'Tautan Putusan'])
-    print(df.head())  # Menampilkan beberapa baris pertama dari DataFrame
-except Exception as e:
-    print(f"Error reading the Excel file: {e}")
